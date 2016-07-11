bbe983a7d296a0f9920506b212dd69ffbdcbaa95
root <root@localhost.(none)>
2016-07-11 14:29:05 -0400

    Base schema completed. TODOs still remain throughout.

--- EagerAcademy/imports/api/assignments/assignments.js
+++ EagerAcademy/imports/api/assignments/assignments.js
@@ -7,3 +7,78 @@ import faker from 'faker';
 import { Students } from '../../students/students.js';
 import { Courses } from '../../courses/courses.js';
 import { Teachers } from '../../teachers/teachers.js';
+
+/*
+--::TODO::--
+*/
+//Mongo Methods
+class AssignmentsCollection extends Mongo.Collection {
+  
+  
+}
+
+export const Assignments = new AssignmentsCollection('Assignments');
+
+Assignments.deny({
+  //Denial of client-side updates, instead will use methods to manage this collection
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+});
+
+Assignments.schema = new SimpleSchema({
+  _id: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    denyUpdate: true,
+  },
+  courseId: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    denyUpdate: true,
+  },
+  name: { type: String, },
+  startDate: {
+    type: Date,
+  },
+  dueDate: {
+    type: Date,
+  },
+  studentComments: {
+    type: String,
+    max: 500,
+  },
+  teacherComments: {
+    type: String,
+    max: 500,
+  },
+});
+
+Assignments.attachSchema(Assignments.schema);
+
+// This represents the keys from Course objects that should be published to the client.
+Assignments.publicFields = {
+  _id: 1,
+  courseId: 1,
+  name: 1,
+  startDate: 1,
+  dueDate: 1,
+  studentComments: 1,
+  teacherComments: 1,
+};
+
+/*
+--::TODO::--
+*/
+// Factory for defining assignment
+Factory.define('assignment', Assignments, {
+  
+});
+
+/*
+--::TODO::--
+*/
+// Helpers - Getter/Setter
+Assignments.helpers({
+  
+});
