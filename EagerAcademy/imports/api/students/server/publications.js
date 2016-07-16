8305d26fe6604c31cc4b3b6aebec7c1eae76008f
GwFreak01 <gwfreak01@gmail.com>
2016-07-16 19:00:56 -0400

    Merging Local with Github

---
+++ EagerAcademy/imports/api/students/server/publications.js
@@ -0,0 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+import { Students } from '../students.js';
+import { Courses } from '../../courses/courses.js';
+import { Teachers } from '../../teachers/teachers.js';
+
